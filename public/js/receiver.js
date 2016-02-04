29260a8cee02e4ceb31072f27f642933267fa2a2
Amr Gamal <amr.gamal878@gmail.com>
2016-02-04 18:34:39 +0200

    reset

--- public/js/receiver.js
+++
@@ -1,8 +0,0 @@
-var socket = new io.connect('http://localhost:3000');
-
-socket.on('user-login',function(data){
-//    var d = JSON.parse(data);
-//    alert(d.username);
-});
-
-
--- resources/views/partials/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-    <!-- To the right -->
-    <div class="pull-right hidden-xs">
-      Artistic Touch
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; {{date("Y")}} <a href="#">Artisan</a>.</strong> All rights reserved.
-  </footer>

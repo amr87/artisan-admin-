79718460cecace3c3812e5c2c0a5a8ca558f6fbd
Amr Gamal <amr.gamal878@gmail.com>
2016-02-16 00:06:11 +0200

    responsive datatable

--- resources/views/admin/users/all.blade.php
+++ resources/views/admin/users/all.blade.php
@@ -32,7 +32,7 @@
 
         @endif
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <table id="users" class="table table-striped table-bordered">
+            <table id="users" class="table table-striped table-bordered table-responsive">
                 <thead>
                     <tr>
                         <th>Username</th>
